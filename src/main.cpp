dbd9798b77ba19f76d27b8be80989876662e3489
Minevitably <plotmax@163.com>
2025-04-14 21:28:19 +0800

    删除一些无意义代码

--- src/main.cpp
+++ src/main.cpp
@@ -5,32 +5,6 @@
 
 int main(int argc, char *argv[])
 {
-    // 使用默认配置路径初始化
-    // DBUtil::initConfig(); // 可选，getConnection()会自动初始化
-
-    // 获取数据库连接
-    QSqlDatabase db = DBUtil::getConnection();
-    if (db.isValid()) {
-        QSqlQuery query(db);
-        if (query.exec("SELECT * FROM users")) {
-            while (query.next()) {
-                // 处理查询结果...
-                int id = query.value(0).toInt();
-                QString username = query.value(1).toString();
-                QString name = query.value(2).toString();
-                QString type = query.value(3).toString();
-
-                qDebug() << id << "\t" << username << "\t" << name << "\t" << type;
-            }
-        } else {
-            qDebug() << "Query error:" << query.lastError().text();
-        }
-        db.close();
-    } else {
-        qDebug() << "Failed to get database connection";
-    }
-
-
     QApplication a(argc, argv);
     Widget w;
     w.show();
--- src/widget.cpp
+++ src/widget.cpp
@@ -1,5 +1,6 @@
 #include "widget.h"
 #include "ui_widget.h"
+#include "dbutil.h"
 
 Widget::Widget(QWidget *parent)
         : QWidget(parent)
@@ -7,70 +8,37 @@ Widget::Widget(QWidget *parent)
 {
     ui->setupUi(this);
 
-    // 初始化时连接数据库并查询
-    if(connectToDatabase()) {
-//        queryUsers();
-    } else {
-        qDebug() << "Failed to connect to database!";
-    }
-
     connect(ui->pushButton, &QPushButton::clicked, this, &Widget::onPushButtonClicked);
 }
 
 Widget::~Widget()
 {
-    // 关闭数据库连接
-    if(db.isOpen()) {
-        db.close();
-    }
     delete ui;
 }
 
-bool Widget::connectToDatabase()
-{
-    // 配置数据库连接
-    db = QSqlDatabase::addDatabase("QMYSQL");
-    db.setHostName("172.24.160.10");      // 数据库服务器地址
-    db.setDatabaseName("library_management");  // 数据库名
-    db.setUserName("root");           // 数据库用户名
-    db.setPassword("123456");       // 数据库密码
-
-    if(!db.open()) {
-        QSqlError error = db.lastError();
-        qDebug() << "Database error occurred:" << error.text();
-        qDebug() << "Database error occurred:" << db.lastError().text();
-        return false;
-    }
-
-    qDebug() << "Connected to database successfully!";
-    return true;
-}
-
-void Widget::queryUsers()
-{
-    QSqlQuery query;
-    QString sql = "SELECT id, username, name, user_type FROM users";
-
-    if(!query.exec(sql)) {
-        qDebug() << "Query failed:" << query.lastError().text();
-        return;
-    }
-
-    qDebug() << "User list:";
-    qDebug() << "ID\tUsername\tName\tType";
-
-    while(query.next()) {
-        int id = query.value(0).toInt();
-        QString username = query.value(1).toString();
-        QString name = query.value(2).toString();
-        QString type = query.value(3).toString();
-
-        qDebug() << id << "\t" << username << "\t" << name << "\t" << type;
-    }
-}
 
 void Widget::onPushButtonClicked()
 {
     ui->label->setText("Database Connected!");
-    queryUsers();  // 点击按钮时重新查询
+    // 获取数据库连接
+    QSqlDatabase db = DBUtil::getConnection();
+    if (db.isValid()) {
+        QSqlQuery query(db);
+        if (query.exec("SELECT * FROM users")) {
+            while (query.next()) {
+                // 处理查询结果...
+                int id = query.value(0).toInt();
+                QString username = query.value(1).toString();
+                QString name = query.value(2).toString();
+                QString type = query.value(3).toString();
+
+                qDebug() << id << "\t" << username << "\t" << name << "\t" << type;
+            }
+        } else {
+            qDebug() << "Query error:" << query.lastError().text();
+        }
+        db.close();
+    } else {
+        qDebug() << "Failed to get database connection";
+    }
 }
