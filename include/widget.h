8b184ab8a6fd5c81b30d8f4bce71817bf3bdc55d
Minevitably <plotmax@163.com>
2025-04-14 21:29:06 +0800

    删除一些无意义代码

--- include/widget.h
+++ include/widget.h
@@ -19,14 +19,10 @@ public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
 
-    bool connectToDatabase();  // 数据库连接方法
-    void queryUsers();         // 查询用户方法
-
 private slots:
     void onPushButtonClicked();
 
 private:
     Ui::Widget *ui;
-    QSqlDatabase db;  // 数据库连接对象
 };
 #endif // WIDGET_H
